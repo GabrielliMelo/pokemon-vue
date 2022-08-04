2f0d26ccb4c58be8109d0220e4157424a63c3667
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-08-04 12:58:26 -0300

    feat: fetch na rota de buscar 1 pokemon

--- src/components/PogePokemon/PagePokemon.vue
+++ src/components/PogePokemon/PagePokemon.vue
@@ -1,9 +1,44 @@
 <template>
   <div id="container">
+    <header-poke></header-poke>
+
     <button><router-link :to="{ name: 'home' }">Voltar</router-link></button>
   </div>
 </template>
 
-<script></script>
+<script>
+import Header from "../shared/Header/Header.vue";
 
-<style></style>
+export default {
+  components: {
+    "header-poke": Header,
+  },
+  data() {
+    return {
+      poke: this.$route.params.poke,
+      pokemon: {},
+    };
+  },
+  created() {
+    this.$http
+      .get(`https://back-pokemon-simbiox.herokuapp.com/pokemons/${this.poke}`)
+      .then((res) => res.json())
+      .then(
+        (pokemon) => {
+          this.pokemon = pokemon;
+          console.log(pokemon);
+        },
+        (err) => console.log(err)
+      );
+  },
+};
+</script>
+
+<style>
+#container {
+  background-image: url("../../assets/place.png");
+  min-height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
